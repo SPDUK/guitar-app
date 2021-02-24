e43440f51c6ffd21ac8ad01ccc08299a28d544ee
Stephen Parker <spdevuk@gmail.com>
2021-02-24 22:34:13 +0000

    feat: add gear list to home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 import {
-  home, bgHeader, bgHeaderTwo, bgHeaderThree, text, actions,
+  home, bgHeader, bgHeaderTwo, bgHeaderThree, text, actions, gear, gearItems,
 } from './home.module.scss';
 import { ReactComponent as BgHeader } from '../../images/bg-header.svg';
 import { ReactComponent as BgHeaderTwo } from '../../images/bg-header-2.svg';
@@ -24,15 +24,29 @@ export default function Home() {
 
         <div className={actions}>
           <Link to="/songs">
-            <Button type="primary">
+            <Button type="primary" size="large">
               View Songs
             </Button>
           </Link>
           <Link to="/chords">
-            <Button type="primary">
+            <Button size="large">
               View Chords
             </Button>
           </Link>
+        </div>
+
+        <div className={gear}>
+          <h2>What I use:</h2>
+          <div className={gearItems}>
+            <div>
+              <a href="https://www.ibanez.com/eu/products/detail/grg170dx_2y_04.html">Guitar: Ibanez GRG170DX</a>
+              <a href="https://focusrite.com/en/usb-audio-interface/scarlett/scarlett-solo">Audio interface: Focusrite Solo</a>
+            </div>
+            <div>
+              <a href="https://www.stltones.com/products/stl-ignite-emissary-plug-in-bundle">Amp Sim: STL Ignite</a>
+              <a href="https://en-uk.sennheiser.com/hd-560-s-audiophile-headphone-high-end-over-ear">Headphones: Sennheiser HD 560S</a>
+            </div>
+          </div>
 
         </div>
       </div>
